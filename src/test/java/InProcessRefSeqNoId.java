da4d289e551c2ac4b66cef4b7367f5f484862c64
akshaya15g <95008247+akshaya15g@users.noreply.github.com>
2023-03-23 11:11:01 +0100

    Add files via upload
    This test case is created using the older version of hibernate where everything works fine

---
+++ src/test/java/InProcessRefSeqNoId.java
@@ -0,0 +1,31 @@
+import javax.persistence.Column;
+import javax.persistence.Id;
+
+import java.io.Serializable;
+
+
+public class InProcessRefSeqNoId implements Serializable {
+
+    private String refNo;
+    private String seqNo;
+
+    @Column(name = "REF_NO")
+    @Id
+    public String getRefNo() {
+        return refNo;
+    }
+
+    public void setRefNo(String refNo) {
+        this.refNo = refNo;
+    }
+
+    @Column(name = "SEQ_NO")
+    @Id
+    public String getSeqNo() {
+        return seqNo;
+    }
+
+    public void setSeqNo(String seqNo) {
+        this.seqNo = seqNo;
+    }
+}
